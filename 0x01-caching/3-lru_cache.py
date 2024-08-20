4d3cbfe7baccf99657d76268d229eaf426683da6
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-20 23:41:22 +0300

    Add 3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+This module contains the LRUCache class
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    A caching system
+    """
+    def __init__(self):
+        """
+        Calls the parent init and instantiate the self.keys list
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.keys.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed_key = self.keys.pop(0)
+                del self.cache_data[removed_key]
+                print('DISCARD: {}'.format(removed_key))
+
+            self.cache_data[key] = item
+            self.keys.append(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key in self.cache_data:
+            self.keys.remove(key)
+            self.keys.append(key)
+        return self.cache_data.get(key)
