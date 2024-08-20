41b9233c31ca21172005f645874379c1411b8ae3
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-20 19:47:28 +0300

    Add 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-This module contains the FIFOCache class
+This module contains the LIFOCache class
 """
 
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """
     A caching system
     """
@@ -22,14 +22,17 @@ class FIFOCache(BaseCaching):
         Add an item in the cache
         """
         if key is not None and item is not None:
-            self.cache_data[key] = item
-            self.keys.append(key)
+            if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data:
+                    k = self.keys[-1]
+                    del self.cache_data[k]
+                    self.keys.pop()
+                    print('DISCARD: {}'.format(k))
 
-            if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
-                k = self.keys[0]
-                del self.cache_data[k]
-                self.keys.pop(0)
-                print('DISCARD: {}'.format(k))
+            self.cache_data[key] = item
+            if key in self.keys:
+                self.keys.remove(key)
+            self.keys.append(key)
 
     def get(self, key):
         """
